98b53d33272d6b75a99bcdc975cc0859678304ab
PraneethMalshan <praneethmalshan12345@gmail.com>
2023-10-27 20:36:47 +0530

    Added admin dashboard and car details and table

--- FrontEnd/assests/scripts/AdminDashboard.js
+++ FrontEnd/assests/scripts/AdminDashboard.js
@@ -20,3 +20,63 @@ toggle.onclick = function () {
     main.classList.toggle("active");
 };
 
+
+    document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("DashboardF").style.display = 'inline-block';
+    document.getElementById("CarF").style.display = 'none';
+    document.getElementById("CustomerF").style.display = 'none';
+    document.getElementById("DriverF").style.display = 'none';
+    document.getElementById("CarRentalF").style.display = 'none';
+    document.getElementById("PaymentF").style.display = 'none';
+});
+
+    document.getElementById("DashboardForm").addEventListener('click', function () {
+    document.getElementById("DashboardF").style.display = 'inline-block';
+    document.getElementById("CarF").style.display = 'none';
+    document.getElementById("CustomerF").style.display = 'none';
+    document.getElementById("DriverF").style.display = 'none';
+    document.getElementById("CarRentalF").style.display = 'none';
+    document.getElementById("PaymentF").style.display = 'none';
+});
+
+    document.getElementById("CarForm").addEventListener('click', function () {
+    document.getElementById("DashboardF").style.display = 'none';
+    document.getElementById("CarF").style.display = 'inline-block';
+    document.getElementById("CustomerF").style.display = 'none';
+    document.getElementById("DriverF").style.display = 'none';
+    document.getElementById("CarRentalF").style.display = 'none';
+    document.getElementById("PaymentF").style.display = 'none';
+});
+    document.getElementById("CustomerForm").addEventListener('click', function () {
+    document.getElementById("DashboardF").style.display = 'none';
+    document.getElementById("CarF").style.display = 'none';
+    document.getElementById("CustomerF").style.display = 'inline-block';
+    document.getElementById("DriverF").style.display = 'none';
+    document.getElementById("CarRentalF").style.display = 'none';
+    document.getElementById("PaymentF").style.display = 'none';
+});
+    document.getElementById("DriverForm").addEventListener('click', function () {
+    document.getElementById("DashboardF").style.display = 'none';
+    document.getElementById("CarF").style.display = 'none';
+    document.getElementById("CustomerF").style.display = 'none';
+    document.getElementById("DriverF").style.display = 'inline-block';
+    document.getElementById("CarRentalF").style.display = 'none';
+    document.getElementById("PaymentF").style.display = 'none';
+});
+    document.getElementById("CarRentalForm").addEventListener('click', function () {
+    document.getElementById("DashboardF").style.display = 'none';
+    document.getElementById("CarF").style.display = 'none';
+    document.getElementById("CustomerF").style.display = 'none';
+    document.getElementById("DriverF").style.display = 'none';
+    document.getElementById("CarRentalF").style.display = 'inline-block';
+    document.getElementById("PaymentF").style.display = 'none';
+});
+    document.getElementById("PaymentForm").addEventListener('click', function () {
+    document.getElementById("DashboardF").style.display = 'none';
+    document.getElementById("CarF").style.display = 'none';
+    document.getElementById("CustomerF").style.display = 'none';
+    document.getElementById("DriverF").style.display = 'none';
+    document.getElementById("CarRentalF").style.display = 'none';
+    document.getElementById("PaymentF").style.display = 'inline-block';
+});
+
