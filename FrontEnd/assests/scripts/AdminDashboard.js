904c4a0a347662ea9d0b63b866a58894b9559de9
PraneethMalshan <praneethmalshan12345@gmail.com>
2023-10-28 18:59:06 +0530

    Added Admin panel's IncomeReport page and Rental request page

--- FrontEnd/assests/scripts/AdminDashboard.js
+++ FrontEnd/assests/scripts/AdminDashboard.js
@@ -28,6 +28,8 @@ toggle.onclick = function () {
     document.getElementById("DriverF").style.display = 'none';
     document.getElementById("CarRentalF").style.display = 'none';
     document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
 });
 
     document.getElementById("DashboardForm").addEventListener('click', function () {
@@ -37,6 +39,8 @@ toggle.onclick = function () {
     document.getElementById("DriverF").style.display = 'none';
     document.getElementById("CarRentalF").style.display = 'none';
     document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
 });
 
     document.getElementById("CarForm").addEventListener('click', function () {
@@ -46,6 +50,8 @@ toggle.onclick = function () {
     document.getElementById("DriverF").style.display = 'none';
     document.getElementById("CarRentalF").style.display = 'none';
     document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
 });
     document.getElementById("CustomerForm").addEventListener('click', function () {
     document.getElementById("DashboardF").style.display = 'none';
@@ -54,6 +60,8 @@ toggle.onclick = function () {
     document.getElementById("DriverF").style.display = 'none';
     document.getElementById("CarRentalF").style.display = 'none';
     document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
 });
     document.getElementById("DriverForm").addEventListener('click', function () {
     document.getElementById("DashboardF").style.display = 'none';
@@ -62,6 +70,8 @@ toggle.onclick = function () {
     document.getElementById("DriverF").style.display = 'inline-block';
     document.getElementById("CarRentalF").style.display = 'none';
     document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
 });
     document.getElementById("CarRentalForm").addEventListener('click', function () {
     document.getElementById("DashboardF").style.display = 'none';
@@ -70,6 +80,8 @@ toggle.onclick = function () {
     document.getElementById("DriverF").style.display = 'none';
     document.getElementById("CarRentalF").style.display = 'inline-block';
     document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
 });
     document.getElementById("PaymentForm").addEventListener('click', function () {
     document.getElementById("DashboardF").style.display = 'none';
@@ -78,5 +90,27 @@ toggle.onclick = function () {
     document.getElementById("DriverF").style.display = 'none';
     document.getElementById("CarRentalF").style.display = 'none';
     document.getElementById("PaymentF").style.display = 'inline-block';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'none';
+});
+    document.getElementById("RentalRequestForm").addEventListener('click', function () {
+    document.getElementById("DashboardF").style.display = 'none';
+    document.getElementById("CarF").style.display = 'none';
+    document.getElementById("CustomerF").style.display = 'none';
+    document.getElementById("DriverF").style.display = 'none';
+    document.getElementById("CarRentalF").style.display = 'none';
+    document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'inline-block' ;
+    document.getElementById("IncomeReportF").style.display = 'none';
+});
+    document.getElementById("IncomeReportForm").addEventListener('click', function () {
+    document.getElementById("DashboardF").style.display = 'none';
+    document.getElementById("CarF").style.display = 'none';
+    document.getElementById("CustomerF").style.display = 'none';
+    document.getElementById("DriverF").style.display = 'none';
+    document.getElementById("CarRentalF").style.display = 'none';
+    document.getElementById("PaymentF").style.display = 'none';
+    document.getElementById("RentalRequestF").style.display = 'none';
+    document.getElementById("IncomeReportF").style.display = 'inline-block';
 });
 
