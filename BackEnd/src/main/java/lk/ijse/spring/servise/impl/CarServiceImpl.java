35e022c89c5f7ca85bef0b2305c251c86f983065
PraneethMalshan <praneethmalshan12345@gmail.com>
2023-11-01 03:39:53 +0530

    Added CustomerServiceImpl Save part

--- BackEnd/src/main/java/lk/ijse/spring/servise/impl/CarServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/servise/impl/CarServiceImpl.java
@@ -1,15 +1,31 @@
 package lk.ijse.spring.servise.impl;
 
 import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.entity.Car;
+import lk.ijse.spring.repo.CarRepo;
 import lk.ijse.spring.servise.CarService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 
+@Service
+@Transactional
 public class CarServiceImpl implements CarService {
+    @Autowired
+    private CarRepo repo ;
+
+    @Autowired
+    private ModelMapper mapper ;
 
     @Override
     public void saveCar(CarDTO dto) {
-
+        if (repo.existsById(dto.getRegistrationNO())){
+            throw new RuntimeException("Customer already Exists.. Please enter another ID..");
+        }
+        repo.save(mapper.map(dto, Car.class));
     }
 
     @Override
