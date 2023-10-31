7e3d337908ffd63c605eecdf836b047bd7e3896a
PraneethMalshan <praneethmalshan12345@gmail.com>
2023-11-01 02:27:23 +0530

    Added Customer frontEnd controller

--- FrontEnd/controllers/Admin/CarController.js
+++ FrontEnd/controllers/Admin/CarController.js
@@ -1,7 +1,31 @@
 let baseUrl="http://localhost:8081/easycarrental/"
 
 
+/*Save Car*/
+$("#btnSaveCar").click(function () {
+   let formData = $("#carForm").serialize();
 
+   $.ajax({
+       url:baseUrl+"car",
+       method:"post",
+       data:formData,
+       dataType: "json",
+       success:function (res) {
+           //Invoked if the response status code is in 200 range
+           console.log("Success Method Invoked");
+           console.log(res);
+           alert(res.message);
+           // getAllCars();
+       },
+       error:function (error) {
+           //Invokes if status code range is 500 range or 400 range
+           console.log("Error Method Invoked");
+           console.log(JSON.parse(error.responseText));
+           alert(JSON.parse(error.responseText).message);
+       }
+   });
+
+});
 
 
 
