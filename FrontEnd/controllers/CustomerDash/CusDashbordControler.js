18d8a4620b4029effcf42083307ed07138d8fbf3
PraneethMalshan <praneethmalshan12345@gmail.com>
2023-11-04 16:21:26 +0530

    Update content

--- FrontEnd/controllers/CustomerDash/CusDashbordControler.js
+++ FrontEnd/controllers/CustomerDash/CusDashbordControler.js
@@ -7,12 +7,12 @@ $("#btnUpdateCustomer").click(function () {
    let password = $('#txtPassword').val();
 
    var customer = {
-      customerId:nicNo,
+      nicNo:nicNo,
       name: name,
       address: address,
       contactNo: contact,
       email: email,
-      nicNo: password
+      password: password
    }
 
    $.ajax({
@@ -70,11 +70,101 @@ function searchCustomerById() {
 function searchCurById(customer) {
    let regisrId = $('#cmbRegistrationNo').find('option:selected').text();
 
-   $.ajax
-
+   $.ajax({
+      url: baseUrl+"car",
+      success:function (res) {
+         for (let c of res.data) {
+            let carID=c.registrationNO;
+            if (carID == regisrId) {
+               let car=c;
+               console.log(car)
+               searchDriverByNic(customer, car)
+            }
+         }
+      },
+      error:function (error) {
+         let message= JSON.parse(error.responseText).message;
+         alert(message);
+      }
+   });
 }
 
 
+function searchDriverByNic(customer, car) {
+   let licenceNo = $('#txtDriverNIC').val();
+   if ($('#txtDriverNIC').val() === "") {
+      licenceNo = null;
+   }
+
+   if (licenceNo != null){
+      $.ajax({
+         url: baseUrl+"driver",
+         success:function (res) {
+            for (let c of res.data) {
+               let drNic=c.nic;
+               if (drNic == licenceNo) {
+                  let driver=c;
+                  console.log(driver)
+                  addCarRent(customer, car, driver);
+               }
+            }
+
+         },
+         error:function (error) {
+            let message= JSON.parse(error.responseText).message;
+            alert(message);
+         }
+
+
+      });
+   } else {
+      addCarRent(customer, car, null);
+
+   }
+
+}
+/*
+rentId advance dayCount pickUpDate rentType status
+*/
+function addCarRent(customer, car, driver) {
+   let rentId = $('#txtCarRentId').val();
+   let advance = $('#txtPaymentAmount').val();
+   let dayCount = $('#txtRantDayCount').val();
+   let pickUpDate = $('#txtCarPickupDate').val();
+   let rentType = $('#cmbRentType').find('option:selected').text();
+   let status = "Pending";
+
+   let carRent = {
+      rentId: rentId,
+      advance: advance,
+      dayCount:dayCount,
+      pickUpDate:pickUpDate,
+      rentType:rentType,
+      status:status,
+      customer:customer,
+      car:car,
+      driver:driver
+   }
+
+   let carData=JSON.stringify(carRent);
+
+   $.ajax({
+      url: baseUrl + "CarRent",
+      method: "post",
+      data: carData,
+      dataType: "json",
+      contentType: "application/json",
+      success: function (rep) {
+         alert(rep.message);
+      },
+      error: function (ob) {
+         alert(ob.message);
+      }
+
+
+   });
+
+}
 
 
 
